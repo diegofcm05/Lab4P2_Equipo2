569efd9ce61401eca3556242df1cd5097bb424f3
Diego Cruz <dfcm950@gmail.com>
2023-08-11 13:47:17 -0600

    Clase Entrenador Actualizada

--- src/lab4p2_equipo2/Lab4P2_Equipo2.java
+++ src/lab4p2_equipo2/Lab4P2_Equipo2.java
@@ -14,6 +14,8 @@ public class Lab4P2_Equipo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Hola");
         // TODO code application logic here
     }
     
