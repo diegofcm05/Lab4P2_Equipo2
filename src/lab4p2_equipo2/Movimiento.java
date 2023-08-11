d848c6305c59ec92d41ddbd9d99357c271a060fb
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-08-11 13:52:42 -0600

    Clase Movimiento creada

---
+++ src/lab4p2_equipo2/Movimiento.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_equipo2;
+
+/**
+ *
+ * @author Junnior Sauceda
+ */
+public class Movimiento {
+    private String nombre, descripcion;
+
+    public Movimiento() {
+    }
+
+    public Movimiento(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Movimiento{" + "Nombre=" + nombre + ", Descripcion=" + descripcion + '}';
+    }
+    
+    
+    
+}
